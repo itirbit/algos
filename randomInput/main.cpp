56daca1f5fc07243239598f6e9905088e96610d4
itirbit <twmw@mail.ru>
2021-01-25 10:47:25 +0000

    add Makefile

--- randomInput/main.cpp
+++ randomInput/main.cpp
@@ -5,10 +5,10 @@ int main(int argc, char** argv)
 	std::fstream fs;
 	fs.open("input.txt", std::fstream::out);
 	srand(time(NULL));
-	int n = rand() % 1000;
+	int n = rand() % 100;
 	for (int i = 0; i < n; ++i)
 	{
-		fs << rand() << ' ';
+		fs << rand()% 1000 << ' ';
 	}
 	fs.close();
 	return 0;
