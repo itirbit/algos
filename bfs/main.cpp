06aaf5ce99da74362f27f28060d8bddaa3d90f52
itirbit <twmw@mail.ru>
2021-02-24 10:35:24 +0000

    add nodes to bfs

--- bfs/main.cpp
+++ bfs/main.cpp
@@ -1,15 +1,6 @@
 #include <queue>
 #include <iostream>
-
-
-struct Node
-{
-	Node():val(0){}
-	Node(int v):val(v){}
-	Node* left{};
-	Node* right{};
-	int val{};
-};
+#include "../nodes/node.h"
 
 void bfs(Node* node)
 {
@@ -29,15 +20,7 @@ void bfs(Node* node)
 
 int main()
 {
-	Node* root = new Node(12);
-	root->left = new Node(7);
-	root->right = new Node(5);
-	root->left->right = new Node(1);
-	root->left->left = new Node(2);
-	bfs(root);
-	delete root->left->left;
-	delete root->left->right;
-	delete root->left;
-	delete root->right;
-	delete root;
+	auto node = getNode();
+	bfs(node);
+	cleanUp(node);
 }
