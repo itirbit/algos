52b3442d09ef87e050bacc756395db98748944df
itirbit <twmw@mail.ru>
2021-02-27 12:03:35 +0000

    some fix

--- dijkstra/main.cpp
+++ dijkstra/main.cpp
@@ -1,6 +1,8 @@
 #include <vector>
 #include <queue>
 #include <map>
+#include <algorithm>
+#include <iostream>
 
 struct Graph
 {
@@ -22,7 +24,8 @@ std::map<int,std::vector<int>> createNeighbours()
 		{2, {1,3,4} },
 		{3, {1,2,4,6} },
 		{4, {2,3} },
-		{5, {4,6} }
+		{5, {4,6} },
+		{6, {1,3,5}}
 	};	
 	return m;
 }
@@ -32,7 +35,9 @@ std::map<int, std::map<int, int>> createDistances()
 	std::map<int, std::map<int, int>> m = {
 		{1, {{2,7}, {3,9}, {6,14}} },
 		{2, {{1,7}, {3,10}, {4,15} }},
-		{3, { } }
+		{3, {{1,9}, {2,10}, {4,11}, {6,2} } },
+		{4, {{2,15}, {3,11}, {5,6} }},
+		{5, {{4,6}, {6,9}}}
 	};	
 	return m;
 }
@@ -40,13 +45,35 @@ std::map<int, std::map<int, int>> createDistances()
 int main()
 {
 	std::vector<Graph*> g;
-	for(int i = 0; i < 5; ++i)
+	auto neigh = createNeighbours();
+	auto dist = createDistances();
+	for(auto [id, nvec] : neigh)
 	{
 		auto graph = new Graph;
-		graph->id = i;
-		graph->neighbours = g;
+		graph->id = id;
 		g.push_back(graph);
 	}
+	for(auto [id, nvec] : neigh)
+	{
+		auto it = std::find_if(g.begin(), g.end(),[id](auto gr){ return gr->id == id;});
+		if (it == g.end())
+		{
+			std::cerr << "failed to dereference graph iterator\n";
+			return 1;
+		}
+		auto graph = *it;
+		for(auto n : nvec)
+		{			
+			auto nit = std::find_if(g.begin(), g.end(),[n](auto gr){ return gr->id == n;});
+			if (nit == g.end())
+			{
+				std::cerr << "failed to dereference neigbour of graph iterator\n";
+				return 1;
+			}
+			graph->neighbours.push_back(*nit);
+		}
+	}
+
 
 	std::queue<Graph*> q;
 	q.push(g.front());
